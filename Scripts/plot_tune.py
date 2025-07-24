d941557760afe4c2819893076bdea4d175b0ba5c
lammyuwuu <lammyllwong@outlook.com>
2025-07-24 10:04:46 +0100

    replaced with mine

--- Scripts/plot_tune.py
+++ Scripts/plot_tune.py
@@ -62,17 +62,10 @@ def tune_calculation_iterator(madx_instance, df_input, cpymad_logfile, sequence_
     
     for index, row in df_input.iterrows():
         
-        twiss_df = cpymad_madx_twiss(madx_instance, cpymad_logfile, sequence_name)  
-        return madx_instance.table.summ.q1[0], madx_instance.table.summ.q2[0]
-
-
-
-    def tune_calculation_iterator(madx_instance, df_input, cpymad_logfile, sequence_name='synchrotron'):
-        qx_array = []  # to store output
-        qy_array = []  # to store output
-        set_qy_array = []
-        qx_out = 0
-        qy_out = 0 
+        # Generate new df of single row
+        df_cut = row
+        try: qx_out, qy_out = set_DW_tunes(madx_instance, cpymad_logfile, row, sequence_name)
+        except: print('skipped point ', row['Qh'], row['Qv'])
         
         # Append value for Machine Qh = qx_out in df_input
         df_input.at[index, 'Machine Qh'] = qx_out
@@ -128,202 +121,89 @@ class resonance_lines(object):
         #ax1.axes.get_yaxis().set_visible(False); 
         #ax1.axes.get_xaxis().set_visible(False);  
         
-        for index, row in df_input.iterrows():
-            
-            # Generate new df of single row
-            df_cut = row
-            try: qx_out, qy_out = set_DW_tunes(madx_instance, cpymad_logfile, row, sequence_name)
-            except: print('skipped point ', row['Qh'], row['Qv'])
-            
-            # Append value for Machine Qh = qx_out in df_input
-            df_input.at[index, 'Machine Qh'] = qx_out
-            # Append value for Machine Qv = qy_out in df_input
-            df_input.at[index, 'Machine Qv'] = qy_out
+        Qx_min = self.Qx_min
+        Qx_max = self.Qx_max
+        Qy_min = self.Qy_min
+        Qy_max = self.Qy_max 
+        ax1.set_xlim(Qx_min, Qx_max);
+        ax1.set_ylim(Qy_min, Qy_max);        
+        ax1.set_xlabel(r'Horizontal Tune $Q_x$')
+        ax1.set_ylabel(r'Vertical Tune $Q_y$')
 
-        return df_input
-
-    class resonance_lines(object):
-
-        def __init__(self, Qx_range, Qy_range, orders, periodicity, legend=False):
-
-            if np.std(Qx_range):
-                self.Qx_min = np.min(Qx_range)
-                self.Qx_max = np.max(Qx_range)
-            else:
-                self.Qx_min = np.floor(Qx_range)-0.05
-                self.Qx_max = np.floor(Qx_range)+1.05
-            if np.std(Qy_range):
-                self.Qy_min = np.min(Qy_range)
-                self.Qy_max = np.max(Qy_range)
-            else:
-                self.Qy_min = np.floor(Qy_range)-0.05
-                self.Qy_max = np.floor(Qy_range)+1.05
-
-            self.periodicity = periodicity
-            self.legend_flag = legend
-
-            nx, ny = [], []
-
-            for order in np.nditer(np.array(orders)):
-                t = np.array(range(-order, order+1))
-                nx.extend(order - np.abs(t))
-                ny.extend(t)
-            nx = np.array(nx)
-            ny = np.array(ny)
-
-            cextr = np.array([nx*np.floor(self.Qx_min)+ny*np.floor(self.Qy_min), \
-                            nx*np.ceil(self.Qx_max)+ny*np.floor(self.Qy_min), \
-                            nx*np.floor(self.Qx_min)+ny*np.ceil(self.Qy_max), \
-                            nx*np.ceil(self.Qx_max)+ny*np.ceil(self.Qy_max)], dtype='int')
-            cmin = np.min(cextr, axis=0)
-            cmax = np.max(cextr, axis=0)
-            res_sum = [range(cmin[i], cmax[i]+1) for i in range(cextr.shape[1])]
-            self.resonance_list = zip(nx, ny, res_sum)
-
-        def plot_resonance_ax(self, ax1):
-            # Remove Borders
-            #ax1.spines['top'].set_visible(False);
-            #ax1.spines['bottom'].set_visible(False);
-            #ax1.spines['left'].set_visible(False);
-            #ax1.spines['right'].set_visible(False);
-            #ax1.axes.get_yaxis().set_visible(False); 
-            #ax1.axes.get_xaxis().set_visible(False);  
-            
-            Qx_min = self.Qx_min
-            Qx_max = self.Qx_max
-            Qy_min = self.Qy_min
-            Qy_max = self.Qy_max 
-            ax1.set_xlim(Qx_min, Qx_max);
-            ax1.set_ylim(Qy_min, Qy_max);        
-            ax1.set_xlabel(r'Horizontal Tune $Q_x$')
-            ax1.set_ylabel(r'Vertical Tune $Q_y$')
-
-            for resonance in self.resonance_list:
-                nx = resonance[0]
-                ny = resonance[1]
-                for res_sum in resonance[2]:
-                    if ny:                    
-                        
-                        if ny%2:                                                  
-                            if res_sum%self.periodicity:
-                                ax1.plot([Qx_min, Qx_max], [(res_sum-nx*Qx_min)/ny, (res_sum-nx*Qx_max)/ny], ls='--', color='b', lw=0.5, label='Non-Systematic Skew')
-                            else:
-                                ax1.plot([Qx_min, Qx_max], [(res_sum-nx*Qx_min)/ny, (res_sum-nx*Qx_max)/ny], ls='--', color='r', lw=1, label='Systematic Skew')               
-                            
-                        else:
-                            if res_sum%self.periodicity:
-                                ax1.plot([Qx_min, Qx_max], [(res_sum-nx*Qx_min)/ny, (res_sum-nx*Qx_max)/ny], color='b', lw=0.5, label='Non-Systematic Normal')
-                            else:
-                                ax1.plot([Qx_min, Qx_max], [(res_sum-nx*Qx_min)/ny, (res_sum-nx*Qx_max)/ny], color='r', lw=1, label='Systematic Normal')
-                    else:                
+        for resonance in self.resonance_list:
+            nx = resonance[0]
+            ny = resonance[1]
+            for res_sum in resonance[2]:
+                if ny:                    
+                    
+                    if ny%2:                                                  
                         if res_sum%self.periodicity:
-                            ax1.plot([float(res_sum)/nx, float(res_sum)/nx],[Qy_min, Qy_max], color='b', lw=0.5, label='Non-Systematic Normal')
+                            ax1.plot([Qx_min, Qx_max], [(res_sum-nx*Qx_min)/ny, (res_sum-nx*Qx_max)/ny], ls='--', color='b', lw=0.5, label='Non-Systematic Skew')
                         else:
-                            ax1.plot([float(res_sum)/nx, float(res_sum)/nx],[Qy_min, Qy_max], color='r', lw=1, label='Systematic Normal')
-        
-            if self.legend_flag: 
-                custom_lines = [Line2D([0], [0], color='r', lw=4),
-                    Line2D([0], [0], color='b', lw=4),
-                    Line2D([0], [2], color='r', lw=4, ls='--'),
-                    Line2D([0], [2], color='b', lw=4, ls='--')]
-                ax1.legend(custom_lines, ['Systematic Normal', 'Non-Systematic Normal', 'Systematic Skew', 'Non-Systematic Skew'])
-        
-        
-        def plot_resonance_fig(self, figure_object = None):
-            plt.ion()
-            if figure_object:
-                fig = figure_object
-                plt.figure(fig.number)
-            else:
-                fig = plt.figure()
-            Qx_min = self.Qx_min
-            Qx_max = self.Qx_max
-            Qy_min = self.Qy_min
-            Qy_max = self.Qy_max 
-            plt.xlim(Qx_min, Qx_max)
-            plt.ylim(Qy_min, Qy_max)
-            plt.xlabel(r'Horizontal Tune $Q_x$')
-            plt.ylabel(r'Vertical Tune $Q_y$')
-            for resonance in self.resonance_list:
-                nx = resonance[0]
-                ny = resonance[1]
-                for res_sum in resonance[2]:
-                    if ny:
-                        line, = plt.plot([Qx_min, Qx_max], \
-                            [(res_sum-nx*Qx_min)/ny, (res_sum-nx*Qx_max)/ny])
+                            ax1.plot([Qx_min, Qx_max], [(res_sum-nx*Qx_min)/ny, (res_sum-nx*Qx_max)/ny], ls='--', color='r', lw=1, label='Systematic Skew')               
+                        
                     else:
-                        line, = plt.plot([float(res_sum)/nx, float(res_sum)/nx],[Qy_min, Qy_max])
-                        
-                        
-                        
-                    if ny%2:
-                        plt.setp(line, linestyle='--') # for skew resonances
+                        if res_sum%self.periodicity:
+                            ax1.plot([Qx_min, Qx_max], [(res_sum-nx*Qx_min)/ny, (res_sum-nx*Qx_max)/ny], color='b', lw=0.5, label='Non-Systematic Normal')
+                        else:
+                            ax1.plot([Qx_min, Qx_max], [(res_sum-nx*Qx_min)/ny, (res_sum-nx*Qx_max)/ny], color='r', lw=1, label='Systematic Normal')
+                else:                
                     if res_sum%self.periodicity:
-                        plt.setp(line, color='b') # non-systematic resonances
+                        ax1.plot([float(res_sum)/nx, float(res_sum)/nx],[Qy_min, Qy_max], color='b', lw=0.5, label='Non-Systematic Normal')
                     else:
-                        plt.setp(line, color='r', linewidth=2.0) # systematic resonances
-            plt.draw()
-            return fig
-        
-        def print_resonances(self):
-            for resonance in self.resonance_list:
-                for res_sum in resonance[2]:
-                    print_string =  '%s %s%s = %s\t%s'%(str(resonance[0]).rjust(2), ("+", "-")[resonance[1]<0], \
-                            str(abs(resonance[1])).rjust(2), str(res_sum).rjust(4), \
-                            ("(non-systematic)", "(systematic)")[res_sum%self.periodicity==0])
-                    print(print_string)
-
-    ######################################################### 
-    #******************* END OF FUNCTIONS AND CLASSES FROM NOTEBOOK ***********************
-    #########################################################
-
-
-
-    ####################################
-    def get_twiss_table():
-        return twiss_0
-    ####################################
-
-
-
-    # 1. Instantiate a MAD-X object
-    cpymad_logfile = 'cpymad_logfile.txt'
-    sequence_name = 'synchrotron'
-
-    madx = cpymad_start(cpymad_logfile)
-
-    #`madx` is the simulation object, next we have to give it the description of the synchrotron, also called the **lattice**
-    #We have a number of lattices to choose from
-
-    # lattice_folder = '../Lattice_Files/00_Simplified_Lattice/'
-    # lattice_folder = '../Lattice_Files/01_Original_Lattice/'
-    # lattice_folder = '../Lattice_Files/02_Aperture_Lattice/'
-    # lattice_folder = '../Lattice_Files/03_CO_Kick_Lattice/'
-    lattice_folder = '../Lattice_Files/04_New_Harmonics/'
-
-    madx.call(file=lattice_folder+'ISIS.injected_beam')
-    madx.call(file=lattice_folder+'ISIS.strength')
-    madx.call(file=lattice_folder+'2023.strength')
-    madx.call(file=lattice_folder+'ISIS.elements')
-    madx.call(file=lattice_folder+'ISIS.sequence')
-
-    cpymad_check_and_use_sequence(madx, cpymad_logfile, sequence_name)
-
-
-
-    # 2. Set the cycle time = beam energy
-    max_E = 800 # 800 MeV
-    cycle_time = 1.5 # 1.5 milliseconds into the 10 ms acceleration cycle of the synchrotron
-
-    twiss_0 = cpymad_madx_twiss(madx, cpymad_logfile, sequence_name)
-
-    cpymad_set_isis_cycle_time(madx, max_E, cycle_time)
-
-
-
-    # 3. Calculate lattice parameters using **TWISS** command
-
-
+                        ax1.plot([float(res_sum)/nx, float(res_sum)/nx],[Qy_min, Qy_max], color='r', lw=1, label='Systematic Normal')
+    
+        if self.legend_flag: 
+            custom_lines = [Line2D([0], [0], color='r', lw=4),
+                Line2D([0], [0], color='b', lw=4),
+                Line2D([0], [2], color='r', lw=4, ls='--'),
+                Line2D([0], [2], color='b', lw=4, ls='--')]
+            ax1.legend(custom_lines, ['Systematic Normal', 'Non-Systematic Normal', 'Systematic Skew', 'Non-Systematic Skew'])
+    
+    
+    def plot_resonance_fig(self, figure_object = None):
+        plt.ion()
+        if figure_object:
+            fig = figure_object
+            plt.figure(fig.number)
+        else:
+            fig = plt.figure()
+        Qx_min = self.Qx_min
+        Qx_max = self.Qx_max
+        Qy_min = self.Qy_min
+        Qy_max = self.Qy_max 
+        plt.xlim(Qx_min, Qx_max)
+        plt.ylim(Qy_min, Qy_max)
+        plt.xlabel(r'Horizontal Tune $Q_x$')
+        plt.ylabel(r'Vertical Tune $Q_y$')
+        for resonance in self.resonance_list:
+            nx = resonance[0]
+            ny = resonance[1]
+            for res_sum in resonance[2]:
+                if ny:
+                    line, = plt.plot([Qx_min, Qx_max], \
+                        [(res_sum-nx*Qx_min)/ny, (res_sum-nx*Qx_max)/ny])
+                else:
+                    line, = plt.plot([float(res_sum)/nx, float(res_sum)/nx],[Qy_min, Qy_max])
+                    
+                    
+                    
+                if ny%2:
+                    plt.setp(line, linestyle='--') # for skew resonances
+                if res_sum%self.periodicity:
+                    plt.setp(line, color='b') # non-systematic resonances
+                else:
+                    plt.setp(line, color='r', linewidth=2.0) # systematic resonances
+        plt.draw()
+        return fig
+    
+    def print_resonances(self):
+        for resonance in self.resonance_list:
+            for res_sum in resonance[2]:
+                print_string =  '%s %s%s = %s\t%s'%(str(resonance[0]).rjust(2), ("+", "-")[resonance[1]<0], \
+                        str(abs(resonance[1])).rjust(2), str(res_sum).rjust(4), \
+                        ("(non-systematic)", "(systematic)")[res_sum%self.periodicity==0])
+                print(print_string)
 
 ######################################################### 
 #******************* END OF FUNCTIONS AND CLASSES FROM NOTEBOOK ***********************
