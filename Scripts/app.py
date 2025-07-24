c9fc7a5dfc9273940cbcf876a9915a96f859ba0b
Mlsrobertson <mlsr0b3rts0n@gmail.com>
2025-07-24 10:47:23 +0100

    commiting

--- Scripts/app.py
+++ Scripts/app.py
@@ -10,7 +10,6 @@ import numpy as np
 
 #### Importing data
 from get_tune_values import *
-from plot_tune import *
 
 st.title("Tune GUI")
 
@@ -22,8 +21,6 @@ st.markdown(
 )
 #  DataFrame
 df = getValues()
-
-# print(df)
 
 set_df = df[df['type'] == 'set']
 
@@ -55,8 +52,9 @@ edited_df = st.data_editor(
     num_rows="dynamic"
 )
 
-################################### tune table
+################################### REPLACE CODE UNDER WITH TABLE
 
+df = getValues()
 
 # plotting set & actual tunes
 fig = px.scatter(df,
@@ -65,7 +63,6 @@ fig = px.scatter(df,
     color="time", 
     symbol="type",
 )
-
 fig.update_layout(
     title="Set & Actual Tunes",
     xaxis_title='Qh',
@@ -75,9 +72,7 @@ fig.update_layout(
 
 st.plotly_chart(fig)
 
-###################################### beta table
 st.title("Beta values table")
-
 
 st.title("Enter time points")
 with st.form(key="form"):
@@ -87,10 +82,11 @@ with st.form(key="form"):
         twiss_table = get_twiss_table(time_point)
 
         fig = px.line(twiss_table, 
-                    x="s", 
-                    y="betx",
-                    labels={
+                     x ="s",
+                     y = "betx",
+                     labels={
                     "betx":"Beta X"
-                    }
-                    )
+                     })
         st.plotly_chart(fig)
+                    
+ 
