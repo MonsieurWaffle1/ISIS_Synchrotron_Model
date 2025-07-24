d8d4f6924b7110e16481006106771966e86d9e4e
Hyperspeed112 <108069507+HyperSpeed112@users.noreply.github.com>
2025-07-24 10:57:28 +0100

    Added function to app.py

--- Scripts/app.py
+++ Scripts/app.py
@@ -11,12 +11,16 @@ import numpy as np
 #### Importing data
 from get_tune_values import *
 
-harmonic_data = pd.read_csv("Collected_EPICS_data/get_EPICS_Harmonics_full_cycle.dat")
-def getHarmonics(cycle):
-    """    Function to get the value of a specific cycle and time from the harmonic data.
-    """
-    values = harmonic_data[(harmonic_data['cycle'] == cycle)]
-    return values
+def get_harmonic(pv_name: str, cycletime: str | float) -> float:
+    """Return rows matching a specific CycleTime and PV value."""
+    # Get the directory of the current script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    csv_path = os.path.join(script_dir, "..", "Collected_EPICS_data", "get_EPICS_Harmonics_full_cycle.dat")
+
+    harmonic_data = pd.read_csv(csv_path)
+
+    harmonic_index = harmonic_data["PV"].str.contains(f"DWTRIM::{pv_name}:AT_TIME:{cycletime}MS")
+    return harmonic_data[harmonic_index]["Harmonic"].to_list()[0]
 
 st.title("Tune GUI")
 
