0db2d97737e045fa8d8e60b6762d2301e553eba5
Mlsrobertson <mlsr0b3rts0n@gmail.com>
2025-07-24 16:36:08 +0100

    commit

--- Scripts/get_tune_values.py
+++ Scripts/get_tune_values.py
@@ -15,7 +15,7 @@ def getValues() -> pd.DataFrame:
     madx = cpymad_start(cpymad_logfile)
 
     # lattice_folder = '..\\Lattice_Files\\04_New_Harmonics\\'
-    lattice_folder = '../Lattice_Files/04_New_Harmonics/'
+    lattice_folder = '/Lattice_Files/04_New_Harmonics/'
 
     madx.call(file=lattice_folder+'ISIS.injected_beam')
     madx.call(file=lattice_folder+'ISIS.strength')
@@ -85,5 +85,3 @@ def getValues() -> pd.DataFrame:
 
 print(getValues())
 # output_df = tune_calculation_iterator(madx, tq_currents_df, cpymad_logfile)
-
-
